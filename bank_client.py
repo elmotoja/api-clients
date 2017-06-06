97518bbdb67968bf1058135f2833f84088414e6c
Pawel Stelmach <elmotoja@gmail.com>
2017-06-06 18:25:10 +0200

    added logging in files

--- bank_client.py
+++ bank_client.py
@@ -2,6 +2,8 @@ import requests # http://docs.python-requests.org/en/latest/
 import helper_functions as h
 from datetime import datetime, timedelta
 
+# TODO: add log-files to gitignore 
+
 TABELE = ['a', 'b']
 CURRENCY_CODES = h.import_country_codes('ISO4217_codes.csv')
 # print(CURRENCY_CODES)
@@ -13,6 +15,9 @@ date_from = date_to - timedelta(days = 180)
 intervals = h.divide_interval_into_smaller_pieces(date_from, date_to)
 
 success = False
+error_log = open('log-files/error_log.txt', 'w')
+results_log = open('log-files/results_log.txt', 'w')
+results_json = open('log-files/results_json.json', 'w')
 
 for code in CURRENCY_CODES:
     for interval in intervals:
@@ -20,18 +25,17 @@ for code in CURRENCY_CODES:
         date_to_s = interval[1].strftime("%Y-%m-%d")
         try:
             url = 'http://api.nbp.pl/api/exchangerates/rates/' + TABELE[0] + '/'+ code + '/' + date_from_s + '/' + date_to_s + '/'
-            # print(url)
             r = requests.get(url)
             r.raise_for_status()
         except requests.exceptions.HTTPError:
-            # TODO: log error to file
-            print('error:', r.status_code, 'for code', code)
+            # log error to file
+            print('error:', r.status_code, 'for code', code, file=error_log)
         else:
             result = r.json()
-            print(result)
             success = True
     if success:
-        print("success for code:", code)
+        # save the result in the 'log-files' folder
+        print("success for code:", code, file=results_log)
+        print(result, file=results_log)
+        print(result, file=results_json)
         break
-
-# save the result in the 'results' folder
--- helper_functions.py
+++ helper_functions.py
@@ -12,11 +12,7 @@ def import_country_codes(file):
 
 
 def divide_interval_into_smaller_pieces(date_from, date_to, number_of_days_in_interval = 90):
-    # liczba_dni = 14 # (tounix(data_do) - tounix(data_do))/4000
-    # liczba_okresow_90 = int(liczba_dni / 90)
     intervals = []
-    # date_from = parse(date_from)
-    # date_to = parse(date_to)
     now = date_from
 
     while now < date_to:
@@ -26,30 +22,13 @@ def divide_interval_into_smaller_pieces(date_from, date_to, number_of_days_in_in
 
     return intervals
 
-    # now = datetime.datetime.now()
-    # print(now)
-    # print(now.year)
-    # print(now.hour)
 
-    
-    # liczba_dni = 14 # (tounix(data_do) - tounix(data_do))/4000
-    # liczba_okresow_90 = int(liczba_dni / 90)
-    # kawalki = []
+# # test
+# date_to = datetime.now()
+# date_from = date_to - timedelta(days = 180)
+# intervals = divide_interval_into_smaller_pieces(date_from, date_to)
 
-    # for i in range(liczba_okresow_90):
-    #     kawalek = [date_from + i*90,date_from+(i+1)*90]
-    #     kawalki.append(kawalek)
-
-    # return kawalki
-
-
-
-# test
-date_to = datetime.now()
-date_from = date_to - timedelta(days = 180)
-intervals = divide_interval_into_smaller_pieces(date_from, date_to)
-
-for interval in intervals:
-    print(interval[0])
-    print(interval[1])
-    print('----')
+# for interval in intervals:
+#     print(interval[0])
+#     print(interval[1])
+#     print('----')
